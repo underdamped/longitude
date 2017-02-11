975964f70ab3577f8dfe4ca3c75fcc481cea99e9
Javier Lombillo <javier@asymptotic.org>
2017-02-11 12:55:13 -0500

    Button pin assigment
    Updated pin assignment to reflect the new PCB layout.  Kept the measure
    button associated with pin 5 to be compatible with old board.

--- longitude_buttons.cpp
+++ longitude_buttons.cpp
@@ -20,16 +20,12 @@ void ISR_DN(void);
 void button_setup(void)
 {
   //declare button pins
-  b_measure.pin = 5;                  
-  b_mode.pin= 6;
-  b_UP.pin = 23;
-  b_DN.pin = 22;
 
   //initialize buttons as inactive 
   b_measure.state = INACTIVE;
-  b_mode.state = INACTIVE;
-  b_UP.state = INACTIVE;
-  b_DN.state = INACTIVE;
+  b_mode.state    = INACTIVE;
+  b_UP.state      = INACTIVE;
+  b_DN.state      = INACTIVE;
 
   pinMode(b_measure.pin, INPUT_PULLUP);
   pinMode(b_mode.pin, INPUT_PULLUP);
@@ -37,10 +33,10 @@ void button_setup(void)
   pinMode(b_DN.pin, INPUT_PULLUP);
   
   //every button is an interrupt
-  attachInterrupt(b_measure.pin,ISR_measure,LOW);
-  attachInterrupt(b_mode.pin,ISR_mode,LOW);
-  attachInterrupt(b_UP.pin,ISR_UP,LOW);
-  attachInterrupt(b_DN.pin,ISR_DN,LOW);
+  attachInterrupt( b_measure.pin, ISR_measure, LOW );
+  attachInterrupt( b_mode.pin, ISR_mode, LOW );
+  attachInterrupt( b_UP.pin, ISR_UP, LOW );
+  attachInterrupt( b_DN.pin, ISR_DN, LOW );
 }
 
 void ISR_measure(void)
