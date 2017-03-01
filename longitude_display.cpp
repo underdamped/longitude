920d66f540524abce0958bbeee142fe8215e47c8
momonarie22 <momonarie22@gmail.com>
2017-02-28 19:56:29 -0500

    Revert "display update"
    This reverts commit f6ffcb472a366e0d91a1a3f55dfdbad082fd856c.

--- longitude_display.cpp
+++ longitude_display.cpp
@@ -6,18 +6,13 @@
  */
 #include "longitude.h"
 #include "Adafruit_GFX.h"
-#include "Adafruit_HX8357.h"
-#include "gfxfont.h"
-#include <Fonts/FreeMonoOblique9pt7b.h>
-#include <Fonts/FreeSerifItalic9pt7b.h>
-#include <Fonts/FreeMono12pt7b.h>
-#include <Fonts/FreeSans12pt7b.h>
-
+#include "Adafruit_ILI9341.h"
+//#include "SPI.h"
 
 // TFT setup
-#define TFT_CS 15
 #define TFT_DC 20
-Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC);
+#define TFT_CS 15
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 static struct laser laser_left;
 static struct laser laser_right;
@@ -30,8 +25,8 @@ static void show_measure_screen(void);
 void display_setup(void)
 {
       // initiate Display
-  tft.begin(HX8357D);
-  tft.setRotation(3); //header cables facing left
+    tft.begin();
+    tft.setRotation(1);
 }
 // what we show on the screen depends on our state
 void update_display(void)
@@ -62,34 +57,29 @@ void update_display(void)
 // stubs for moises to do his magic
 static void show_splash_screen(void)
 {
-  //print the team name and draws a red underline
-  tft.setFont(); //default font
-  tft.fillScreen(HX8357_BLACK);  //clear screen
-  tft.setTextColor(HX8357_RED, HX8357_BLACK);
+    //print the team name and draws a red underline
+  tft.fillScreen(ILI9341_BLACK);  
+  tft.setTextColor(ILI9341_RED, ILI9341_BLACK);
   tft.setTextSize(2);
-  tft.setCursor(80,5);
+  tft.setCursor(5,5);
   tft.println("Divide by Zero Electronics");
-  tft.drawFastHLine(2,30,510,HX8357_RED);
+  tft.drawFastHLine(2,30,310,ILI9341_RED);
   
   //device name at center
-  tft.fillTriangle(145,65,20,179,80,179,HX8357_RED); //tilted triangle to simulate the pole of the letter "L"
-  tft.setTextColor(HX8357_YELLOW, HX8357_BLACK);
-  tft.setFont(&FreeSerifItalic9pt7b);
-  tft.setTextSize(4);
-  tft.fillRoundRect(320,160,130,20,10,HX8357_RED);
-  tft.fillRect(80,160,250,20,HX8357_RED);  //red underline 
-  tft.setCursor(125,155);
+  tft.fillTriangle(95,60,50,144,80,144,ILI9341_RED); //tilted triangle to simulate the pole of the letter "L"
+  tft.setTextColor(ILI9341_YELLOW, ILI9341_BLACK);
+  tft.setTextSize(3);
+  tft.setCursor(100,100);
   tft.println("ongitude");
+  tft.fillRect(60,130,200,15,ILI9341_RED);  //red underline 
 
-
-  // Tagline
-  tft.setTextColor(HX8357_WHITE, HX8357_BLACK);
-  tft.setFont(&FreeMonoOblique9pt7b); 
+  // Super-Mega-Special-Awesome Tagline display
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(2);
-  tft.setCursor(45,240);
-  tft.println("Making Measurement");
-  tft.setCursor(75,270);
-  tft.println("a Simple Matter"); 
+  tft.setCursor(40,160);
+  tft.println("Making Measurement a          Simple Matter"); 
+  delay(2500); // appreciate the awesomeness
+  tft.fillScreen(ILI9341_BLACK); //now, move along
     return;
 }
 
@@ -97,84 +87,53 @@ static void show_idle_screen(void)
 {
     // this screen should include the result of the last measurement, if any.
     // (the variable 'measured_length', which stores the result, has scope here)
-  
-  //print the team name and draws a red underline
-  tft.setFont();  // sets default font
-  tft.fillScreen(HX8357_BLACK);  // clear screen
-  tft.setTextColor(HX8357_RED, HX8357_BLACK);
+
+  //Display underlined time name atop the screen  
+  tft.fillScreen(ILI9341_BLACK);  
+  tft.setTextColor(ILI9341_RED, ILI9341_BLACK);
   tft.setTextSize(2);
-  tft.setCursor(80,5);
+  tft.setCursor(5,5);
   tft.println("Divide by Zero Electronics");
-  tft.drawFastHLine(2,30,510,HX8357_RED);
+  tft.drawFastHLine(2,30,310,ILI9341_RED); //red underline
   
   //show last measurement
-  tft.setTextColor(HX8357_WHITE, HX8357_BLACK);
-  tft.setFont(&FreeMono12pt7b);
-  tft.setTextSize(1);
-  tft.setCursor(30,70);
-  tft.println("Last Measurement:");
-  tft.drawRect(10,50,340,100,HX8357_WHITE); 
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(2);
-  tft.setCursor(60,120);
+  tft.setCursor(20,50);
+  tft.println("Last Measurement:");
+  tft.drawRect(10,40,280,60,ILI9341_WHITE); 
+  tft.setTextSize(2);
+  tft.setCursor(50,70);
   tft.println(measured_length);
-  tft.setTextSize(1);
-  tft.setCursor(190,120);
+  tft.setTextSize(2);
+  tft.setCursor(180,70);
   tft.println("Meter(s)");
-  
   //show laser state = OFF
-  tft.setCursor(20,180);
+  tft.setCursor(20,130);
   tft.println("Laser State:  OFF");
-  tft.drawRect(10,160,280,30,HX8357_WHITE);  
-  
+  tft.drawRect(10,120,280,30,ILI9341_WHITE);   
   //show instructions
-  tft.setFont(&FreeMonoOblique9pt7b);
-  tft.setTextSize(1);
-  tft.setCursor(50,250);
+  tft.setTextSize(2);
+  tft.setCursor(50,200);
   tft.println("Click Once to turn");
-  tft.setCursor(90,270);
+  tft.setCursor(90,220);
   tft.println("lasers ON");
     return;
 }
 
 static void show_laser_on_screen(void)
 {
-  //print the team name and draws a red underline
-  tft.setFont();
-  tft.fillScreen(HX8357_BLACK);  
-  tft.setTextColor(HX8357_RED, HX8357_BLACK);
-  tft.setTextSize(2);
-  tft.setCursor(80,5);
-  tft.println("Divide by Zero Electronics");
-  tft.drawFastHLine(2,30,510,HX8357_RED);
-  
-  //show last measurement
-  tft.setTextColor(HX8357_WHITE, HX8357_BLACK);
-  tft.setFont(&FreeMono12pt7b);
-  tft.setTextSize(1);
-  tft.setCursor(30,70);
-  tft.println("Last Measurement:");
-  tft.drawRect(10,50,340,100,HX8357_WHITE); 
-  tft.setTextSize(2);
-  tft.setCursor(60,120);
-  tft.println(measured_length);
-  tft.setTextSize(1);
-  tft.setCursor(190,120);
-  tft.println("Meter(s)");
-  
   //show laser state = ON
-  tft.setTextColor(HX8357_WHITE, HX8357_BLACK);
-  tft.setFont(&FreeMono12pt7b);
-  tft.setTextSize(1);
-  tft.setCursor(20,180);
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  tft.setTextSize(2);
+  tft.setCursor(20,130);
   tft.println("Laser State:  ON ");
-  tft.drawRect(10,160,280,30,HX8357_WHITE); 
-  
+  tft.drawRect(10,120,280,30,ILI9341_WHITE); 
   //show instructions
-  tft.setFont(&FreeMonoOblique9pt7b);
-  tft.setTextSize(1);
-  tft.setCursor(50,250);
+  tft.setTextSize(2);
+  tft.setCursor(50,200);
   tft.println("Click Once to take ");
-  tft.setCursor(70,270);
+  tft.setCursor(70,220);
   tft.println("a measurement   ");
     return;
 }
@@ -186,42 +145,32 @@ static void show_measure_screen(void)
     // processor wakes up, it will be in STATE_IDLE.
 
     //Display underlined time name atop the screen  
-  tft.setFont();
-  tft.fillScreen(HX8357_BLACK);  
-  tft.setTextColor(HX8357_RED, HX8357_BLACK);
+  tft.fillScreen(ILI9341_BLACK);  
+  tft.setTextColor(ILI9341_RED, ILI9341_BLACK);
   tft.setTextSize(2);
-  tft.setCursor(80,5);
+  tft.setCursor(5,5);
   tft.println("Divide by Zero Electronics");
-  tft.drawFastHLine(2,30,510,HX8357_RED);
+  tft.drawFastHLine(2,30,310,ILI9341_RED); //red underline
 
- tft.setTextColor(HX8357_WHITE, HX8357_BLACK);
-  tft.setFont(&FreeSans12pt7b);
-  tft.setTextSize(2);
-  tft.setCursor(130,115);
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  tft.setTextSize(4);
+  tft.setCursor(80,50);
   tft.println("Length:");
-  tft.drawRect(60,70,310,150,HX8357_WHITE);
+  tft.drawFastHLine(50,90,210,ILI9341_RED);
+
+  tft.setTextSize(3);
+  tft.setCursor(50,150);
+  tft.print(measured_length,5);
   tft.setTextSize(2);
-  tft.setCursor(100,170);
-  tft.print(measured_length,3);
-  tft.setTextSize(1);
-  tft.setCursor(230,200);
+  tft.setCursor(180,180);
   tft.println("Meter(s)");
 
 //for debuggin
-  double angle = get_angle();
-  tft.setFont();
+double angle = get_angle();
   tft.setTextSize(2);
-  tft.setCursor(140,250);
+  tft.setCursor(20,210);
   tft.println("Angle: ");
-  tft.setCursor(220,250);
-  tft.print(angle);
-  tft.setCursor(20,280);
-  tft.println("Laser 1: ");
-  tft.setCursor(120,280);
-  tft.print(laser_right.last_measurement,3);
-  tft.setCursor(250,280);
-  tft.println("Laser 2: ");
-  tft.setCursor(350,280);
-  tft.print(laser_left.last_measurement,3);  
+  tft.setCursor(120,210);
+  tft.print(angle);  
     return;
 }
